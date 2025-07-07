5aecdad1328ccf1a99e1917d17912046ee3d9a07
ShioriiCodes <kenzunknowntv@example.com>
2025-07-08 02:25:55 +0800

    Initial Laravel project push

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -25,9 +25,7 @@
                             <p class="text-red-600 text-sm mt-1">{{ $message }}</p>
                         @enderror
                     </div>
-
                     <!-- (other profile fields like name, email, etc.) -->
-
                     <x-primary-button>Save</x-primary-button>
                 </form>
 
@@ -35,7 +33,6 @@
                     @include('profile.partials.update-profile-information-form')
                 </div>
             </div>
-
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-password-form')
